be82810836490d8d7fa39341e997f5b9db7be26f
JamesMusyoka <jamesmu475@gmail.com>
2019-09-20 08:20:11 +0300

    Animations

--- app/src/main/java/com/example/foodapp1/view/main/SplashActivity.java
+++ app/src/main/java/com/example/foodapp1/view/main/SplashActivity.java
@@ -2,29 +2,57 @@ package com.example.foodapp1.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.foodapp1.R;
-
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
+    private ImageView container;
+    private AnimationDrawable animationDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-
-        ImageView imageView = (ImageView)findViewById(R.id.imagerot);
-        TextView textView = (TextView)findViewById(R.id.textrot);
-
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotateanim);
-        imageView.startAnimation(animation);
-
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.downanim);
-        textView.startAnimation(animation1 );
+        container = findViewById(R.id.iv_icons);
+        container.setBackgroundResource(R.drawable.splash_animation);
+        animationDrawable = (AnimationDrawable) container.getBackground();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animationDrawable.start();
+        checkAnimationStatus(150, animationDrawable);
+    }
+    /**
+     * check the animation status recursively, keep the animation until it reach the last frame.
+     *
+     * @param time              period of animation
+     * @param animationDrawable animation list
+     */
+    private void checkAnimationStatus(final int time, final AnimationDrawable animationDrawable) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (animationDrawable.getCurrent() != animationDrawable.getFrame(animationDrawable.getNumberOfFrames() - 1))
+                    checkAnimationStatus(time, animationDrawable);
+                else finish();
+            }
+        }, time);
     }
 }
